43223b43dadbc415fe05b99b6e7c30f1d5cc737f
emanh <cnttblog@gmail.com>
2025-08-07 09:22:46 +0700

    implement basic redis

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -1,9 +1,16 @@
 import Redis from 'ioredis';
+import crypto from 'crypto';
 
 const redis = new Redis({
   host: 'redis',
   port: 6379,
   password: process.env.REDIS_PASSWORD,
 });
+
+export function getCacheKey(destination: string, waitUntil: string, timeout: string) {
+  const keyString = `${destination}_${waitUntil}_${timeout}`;
+  const hash = crypto.createHash('sha256').update(keyString).digest('hex');
+  return `proxy_cache:${hash}`;
+}
 
 export default redis;
